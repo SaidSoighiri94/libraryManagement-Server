b0b33c0b26045b1d3729a3a123192da04317fcba
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-07-26 17:16:23 +0200

    Premeir commit comprennant la creation des packages foctionnels

---
+++ src/main/java/com/soighiri/libraryAp/LibraryRestServerApplication.java
@@ -0,0 +1,13 @@
+package com.soighiri.libraryAp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryRestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soighiri/libraryAp/LibraryRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soighiri.libraryAp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
