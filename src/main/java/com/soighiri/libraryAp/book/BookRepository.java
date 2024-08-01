ed7188d268f205f462d24396bacb870ea109b824
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-08-01 13:11:59 +0200

    Ajout des methodes du crud dans l'interface BookService

--- src/main/java/com/soighiri/libraryAp/book/BookRepository.java
+++ src/main/java/com/soighiri/libraryAp/book/BookRepository.java
@@ -3,4 +3,5 @@ package com.soighiri.libraryAp.book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
+
 }
--- src/main/java/com/soighiri/libraryAp/book/BookService.java
+++ src/main/java/com/soighiri/libraryAp/book/BookService.java
@@ -1,4 +1,12 @@
 package com.soighiri.libraryAp.book;
 
+import java.util.List;
+
 public interface BookService {
+    BookEntity saveBook(BookEntity book);
+    BookEntity updateBook(BookEntity book);
+    void deleteBook(BookEntity book);
+    void  deleteBookById(Long bookId);
+    BookEntity getBookById(Long bookId);
+    List<BookEntity> getAllBooks();
 }
