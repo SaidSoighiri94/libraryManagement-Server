921ae575fda8546263895b4f0570d549c37d0cb3
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-08-01 13:36:04 +0200

    Mis en place de l'implmentation de l'inteface BookService.

--- src/main/java/com/soighiri/libraryAp/book/BookServiceImpl.java
+++ src/main/java/com/soighiri/libraryAp/book/BookServiceImpl.java
@@ -1,4 +1,47 @@
 package com.soighiri.libraryAp.book;
 
-public class BookServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class BookServiceImpl implements BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public BookEntity saveBook(BookEntity book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public BookEntity updateBook(BookEntity book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteBook(BookEntity book) {
+        bookRepository.delete(book);
+
+    }
+
+    @Override
+    public void deleteBookById(Long bookId) {
+        bookRepository.deleteById(bookId);
+    }
+
+    @Override
+    public BookEntity getBookById(Long bookId) {
+        return bookRepository.findById(bookId).get();
+    }
+
+    @Override
+    public List<BookEntity> getAllBooks() {
+        return bookRepository.findAll();
+    }
 }
