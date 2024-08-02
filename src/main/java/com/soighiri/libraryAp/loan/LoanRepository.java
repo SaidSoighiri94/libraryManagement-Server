1b945059921a130fff31666f855675eb38cf5a4d
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-08-02 23:19:29 +0200

    Ajout des methodes personalisées dans l'interface LoanRepositoy

--- src/main/java/com/soighiri/libraryAp/loan/LoanRepository.java
+++ src/main/java/com/soighiri/libraryAp/loan/LoanRepository.java
@@ -4,8 +4,22 @@ import com.soighiri.libraryAp.author.AuthorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface LoanRepository extends JpaRepository<LoanEntity, Long> {
+    // Recherche de tous les prêts dont la date de fin est antérieure à une date donnée.
+    List<LoanEntity> findByEndDateBefore(LocalDate maxEnDate);
+
+    // Recherche de tous les prêts dont la date de fin est postérieure à une date donnée.
+    List<LoanEntity> findByEndDateAfter(LocalDate minEnDate);
+
+    // Recherche de tous les prêts ouverts (statut spécifié) d'un client avec un email donné (insensible à la casse).
+    @Query("SELECT loan FROM LoanEntity loan JOIN loan.pk.customer customer WHERE UPPER(customer.email) = UPPER(?1) AND loan.status =?2 ")
+    List<LoanEntity> getAllOpenLoansOfThisCustomer(String email, LoanStatus status);
+
+    // Recherche d'un prêt spécifique en fonction de l'ID du livre, de l'ID du client et du statut du prêt.
+    @Query("SELECT loan FROM LoanEntity  loan JOIN loan.pk.book book JOIN loan.pk.customer customer WHERE book.bookId = ?1 AND customer.customerId =?2 AND loan.status = ?3")
+    LoanEntity getLoanByCriteria(Long bookId, Long customerId, LoanStatus status);
 
 }
