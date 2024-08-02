20b6cd75fe0e3761872c553b8f1e0e60b63fda74
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-08-03 00:22:21 +0200

    Ajout des methodes sur les interfaces CategoryRepository et CustomerRespositorry

--- src/main/java/com/soighiri/libraryAp/customer/CustomerRepository.java
+++ src/main/java/com/soighiri/libraryAp/customer/CustomerRepository.java
@@ -3,4 +3,5 @@ package com.soighiri.libraryAp.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+    CustomerEntity findByEmail(String email);
 }
