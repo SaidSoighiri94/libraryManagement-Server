1b3f6e010e3ef9158be56a2536513a3891ae0633
SaidSoighiri94 <msaidsoighir@gmail.com>
2024-08-03 13:32:20 +0200

    Impementation du controlleur rest de du domaine customer

--- src/main/java/com/soighiri/libraryAp/customer/CustomerController.java
+++ src/main/java/com/soighiri/libraryAp/customer/CustomerController.java
@@ -1,7 +1,29 @@
 package com.soighiri.libraryAp.customer;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/customer")
 public class CustomerController {
+    private final CustomerService customerService;
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+    @GetMapping(value = "/customers")
+    public List<CustomerEntity> findAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping(value = "/{customerId}")
+    public CustomerEntity getCustomer(@PathVariable Long customerId){
+        return customerService.getCustomerById(customerId);
+    }
+
 }
