f54ad08586c8fb1e214893ef7213295cbd88ff5b
SaidSoighiri94 <msaidsoighir@gmail.com>
2025-02-25 12:25:38 +0100

    Hibernate configuration in the  application.properties file

---
+++ src/main/resources/data/categories.sql
@@ -0,0 +1,14 @@
+INSERT INTO Category values("INF", "Informatique"),
+INSERT INTO Category values("MATH","Mathematique,Physique et chimie"),
+INSERT INTO Category values("ABM","Albums"),
+INSERT INTO Category values("BDE", "Bandes dessinées"),
+INSERT INTO Category values("CON","Contes"),
+INSERT INTO Category values("POE", "Poésies"),
+INSERT INTO Category values("ROM", "Roman et Recis Illustrés"),
+INSERT INTO Category values("THE","Théatre"),
+INSERT INTO Category values("PHI","Philosophie"),
+INSERT INTO Category values("SCI","Science de la vie et de la terre"),
+INSERT INTO Category values("POL","Politique"),
+INSERT INTO Category values("ECO", "Economie et Finance");
+INSERT INTO Category values("JOU","Journal Magazine"),
+INSERT INTO Category values("SCFI", "Science Fiction");
